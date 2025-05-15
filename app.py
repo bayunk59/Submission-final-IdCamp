c189e6bc9f44250e959fd740a741252c70fdf24e
Bayun <bayunk59@gmail.com>
2025-05-15 17:34:57 +0800

    app1

--- app.py
+++ app.py
@@ -75,26 +75,28 @@ with col4:
     
 # Simpan input user ke dictionary (untuk prediksi)
 input_data = {
-    "Debtor": debtor_value,
     "Age_at_enrollment": Age,
     "Gender": gender_value,
     "Scholarship_holder": scholarship_value,
-    "Tuition_fees_up_to_date": tuition_value,
-    "Curricular_units_2nd_sem_approved": Approved2,
-    "Curricular_units_1st_sem_approved": Approved1,
-    "Curricular_units_2nd_sem_grade": Grade2,
-    "Curricular_units_1st_sem_grade": Grade1,
-    "Curricular_units_2nd_sem_evaluations": Evaluations2,
-    "Curricular_units_1st_sem_evaluations": Evaluations1,
-    "Curricular_units_2nd_sem_enrolled": Enrolled2,
     "Curricular_units_1st_sem_enrolled": Enrolled1,
+    "Curricular_units_1st_sem_evaluations": Evaluations1,
+    "Curricular_units_1st_sem_approved": Approved1,
+    "Curricular_units_1st_sem_grade": Grade1,
+    "Curricular_units_2nd_sem_enrolled": Enrolled2,
+    "Curricular_units_2nd_sem_evaluations": Evaluations2,
+    "Curricular_units_2nd_sem_approved": Approved2,
+    "Curricular_units_2nd_sem_grade": Grade2,
+    "Debtor": debtor_value,
+    "Tuition_fees_up_to_date": tuition_value,
 }
 
+input_df = pd.DataFrame([input_data])
+
 with st.expander("View the Raw Data"):
-    st.dataframe(data=data, width=800, height=10)
+    st.dataframe(input_df, width=800, height=10)
     
 if st.button('Predict'):
-    new_data = data_preprocessing(data=data)
+    new_data = data_preprocessing(data=input_df)
     with st.expander("View the Preprocessed Data"):
         st.dataframe(data=new_data, width=800, height=10)
     st.write("Status: {}".format(prediction(new_data)))
